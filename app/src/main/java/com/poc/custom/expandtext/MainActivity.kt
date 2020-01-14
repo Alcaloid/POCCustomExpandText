c3f6aa9210e119582115d990544ae52ad1cafff0
Alcaloid <apichai.1507@mail.kmutt.ac.th>
2020-01-14 09:37:30 +0700

    Commit for change bracnh

--- app/src/main/java/com/poc/custom/expandtext/MainActivity.kt
+++ app/src/main/java/com/poc/custom/expandtext/MainActivity.kt
@@ -12,7 +12,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.poc.custom.expandtext.ui.CustomExpandText
 import kotlinx.android.synthetic.main.activity_main.rvExp
 import kotlinx.android.synthetic.main.item_contain_main.view.containerMain
-import kotlinx.android.synthetic.main.item_sample_expand.view.tvExp
+import kotlinx.android.synthetic.main.item_recommand_detail.view.tvExp
 
 
 class MainActivity : AppCompatActivity() {
@@ -21,17 +21,39 @@ class MainActivity : AppCompatActivity() {
         const val TEXT_NOT_MAX_LINE_ENG = "How best to make use of comments is subject to dispute;"
         const val TEXT_LONG_WITHOUT_NEW_LINE =
             "In computer programming, a comment is a programmer-readable explanation or annotation in the source code of a computer program. They are added with the purpose of making the source code easier for humans to understand, and are generally ignored by compilers and interpreters.[1][2] The syntax of comments in various programming languages varies considerably.Comments are generally formatted as either block comments (also called prologue comments or stream comments) or line comments (also called inline comments).[3]"
-        const val TEXT_WITH_NEW_LINE = "1. Title pageThe only thing difficult about preparing the title page is creating the title itself. \n" +
-                "A good title must be compelling and sufficiently informative to capture the attention of readers. \n" +
-                "Most titles define the main subject, along with the concepts or theories covered in the report. \n" +
-                "Apart from that, the title page also consists of other blocks of information such as the submission date, \n" +
-                "the names and positions of the authors, and the principal reader (or the organization) responsible for preparing the document. \n" +
-                "You may also see writing templates & examples."
-        const val TEXT_NUMBER = "110010011010100010101010101101001111000001111100110101001010111010010101011001011101010010101111001100100110101000101010101011010011110000011111001101010010101110100101010110010111010100101011110011001001101010001010101010110100111100000111110011010100101011101001010101100101110101001010111100110010011010100010101010101101001111000001111100110101001010111010010101011001011101010010101111001100100110101000101010101011010011110000011111001101010010101110100101010110010111010100101011110010101101010101011010101011111001010111001101001101100101101010101010"
-        const val TEXT_ENG_WITHOUT_SPACE = "Afghanistan~Albania~Algeria~Andorra~Angola~Antigua~Barbuda~Argentina~Armenia~Australia~Austria~Azerbaijan~Bahamas~Bahrain~Bangladesh~Barbados~Belarus~Belgium~Belize~Benin~Bhutan~Bolivia~BosniaHerzegovina~Botswana~Brazil~Brunei~Bulgaria~Burkina~Faso~Burundi~Cambodia~Cameroon~Canada~Cape~Verde~Central~African~Republic~Chad~Chile~China~Colombia~Comorros~Congo~Costa~Rica~Cote~dIvoire~Croatia~Cuba~Cyprus~Czech~Republic~Democratic~Republic~of~the~Congo~Denmark~Djibouti~Dominica~Dominican~Republic~East~Timor~Ecuador~Egypt~El~Salvador~Equatorial~Guinea~Eritrea~Estonia~Ethiopia~Fiji~Finland~France~Gabon~Gambia~Georgia~Germany~Ghana~Greece~Grenada~Guatemala~Guinea~Guinea-"
+        const val TEXT_WITH_NEW_LINE =
+            "1. Title pageThe only thing difficult about preparing the title page is creating the title itself. \n" +
+                    "A good title must be compelling and sufficiently informative to capture the attention of readers. \n" +
+                    "Most titles define the main subject, along with the concepts or theories covered in the report. \n" +
+                    "Apart from that, the title page also consists of other blocks of information such as the submission date, \n" +
+                    "the names and positions of the authors, and the principal reader (or the organization) responsible for preparing the document. \n" +
+                    "You may also see writing templates & examples."
+        const val TEXT_NUMBER =
+            "110010011010100010101010101101001111000001111100110101001010111010010101011001011101010010101111001100100110101000101010101011010011110000011111001101010010101110100101010110010111010100101011110011001001101010001010101010110100111100000111110011010100101011101001010101100101110101001010111100110010011010100010101010101101001111000001111100110101001010111010010101011001011101010010101111001100100110101000101010101011010011110000011111001101010010101110100101010110010111010100101011110010101101010101011010101011111001010111001101001101100101101010101010"
+        const val TEXT_ENG_WITHOUT_SPACE =
+            "Afghanistan~Albania~Algeria~Andorra~Angola~Antigua~Barbuda~Argentina~Armenia~Australia~Austria~Azerbaijan~Bahamas~Bahrain~Bangladesh~Barbados~Belarus~Belgium~Belize~Benin~Bhutan~Bolivia~BosniaHerzegovina~Botswana~Brazil~Brunei~Bulgaria~Burkina~Faso~Burundi~Cambodia~Cameroon~Canada~Cape~Verde~Central~African~Republic~Chad~Chile~China~Colombia~Comorros~Congo~Costa~Rica~Cote~dIvoire~Croatia~Cuba~Cyprus~Czech~Republic~Democratic~Republic~of~the~Congo~Denmark~Djibouti~Dominica~Dominican~Republic~East~Timor~Ecuador~Egypt~El~Salvador~Equatorial~Guinea~Eritrea~Estonia~Ethiopia~Fiji~Finland~France~Gabon~Gambia~Georgia~Germany~Ghana~Greece~Grenada~Guatemala~Guinea~Guinea-"
         const val TEXT_ENG_NOT_ENOUGH_SPACE = "Start\n1\n2\n3\n4\n5\nEnd"
         const val TEXT_ENG_NOT_ENOUGH_CHAR = "D\ne\ns\ntina\nt\ni\ni\no\nn"
         const val TEXT_ENG_ENOUGH_MAX_LINE = "How best to make use of comments is\n" +
+                "In computer programming, a comment\n" +
+                "A good title must be compelling \n" +
+                "Apart from that, the title page\n" +
+                "You may also see"
+
+        const val TEXT_NOT_MAX_LINE_THAI = "ไม่เกินบรรทัดแน่นอน"
+        const val TEXT_LONG_WITHOUT_NEW_LINE_THAI =
+            "การนอนหลับเป็นการพักผ่อนที่ดีที่สุดสำหรับทุกคน ตั้งแต่เด็กแรกเกิดจนถึงวัยชราต้องนอนหลับอย่างพอเพียง ในคนปกติมีการเปลี่ยนแปลงของระบบต่างๆของร่างกายที่มีลักษณะเฉพาะ มีการปรับตัวให้เข้ากับกลางวันและกลางคืน โดยใช้วงจรหลับตื่นเป็นตัวกำหนดเพื่อความอยู่รอด การหลับและตื่นมีความแตกต่างกันตามอายุ โดยเปลี่ยนแปลงอย่างช้าๆ แต่ต่อเนื่องตั้งแต่อยู่ในครรภ์มารดาจนถึงวัยชรา เราจะพบว่าทารกแรกเกิดใช้เวลาส่วนใหญ่ไปกับการนอนหลับ วันละประมาณ 16-20 ชั่วโมง เมื่อเข้าสู่วัยเรียนการนอนก็จะลดลงเหลือ 9-10 ชั่วโมง เมื่อเข้าสู่วัยผู้ใหญ่ก็จะใช้เวลาในการนอนเพียง 5-6 ชั่วโมง แต่การนอนหลับของแต่ละคนจะไม่เหมือนกันขึ้นอยู่กับการปฏิบัติตั้งแต่วัยเด็ก เมื่อเข้าสู่วัยชราการนอนหลับจะแตกต่างและเปลี่ยนแปลงอย่างชัดเจน คือเริ่มมีการตื่นในช่วงกลางดึกบ่อยขึ้นจะหลับไม่ได้รวดเดียวถึงเช้าเหมือนวัยหนุ่มสาว อาจมีหลับในช่วงกลางวันเพิ่มมากขึ้นในบางวัน"
+        const val TEXT_WITH_NEW_LINE_THAI = "คอมเม้นต์ครับ \n" +
+                "หากเข้าหลักทับศัพท์แล้วตัวทีจะได้ ต์ เพราะตัวสะกดภาษาอังกฤษที่เป็นตัวทีนั้น ตามหลักแล้วต้องใช้ ต เป็นตัวการันต์เท่านั้น\n" +
+                "จะเป็น ท ได้ก็ต่อเมื่อตัวสะกดภาษาอังกฤษเป็นทีเฮท ครับผม \n" +
+                "และเขาไม่นิยมใส่รูปวรรณยุกต์ ใส่ได้ก็แต่เพียงไม้ไต่คู้เท่านั้น ดังนั้น ไม้โทจึงไม่จำเป็น\n" +
+                "หลักการทับศัพท์จะตกไปทันที หากคำคำนั้นมีบันทึกอยู่ในพจนานุกรมอยู่แล้ว \n" +
+                "แต่คำใดก็ตาม ที่ไม่มีในพจนานุกรมของราชบัณฑิตฯ ก็ต้องมาเข้ากระบวนการทับศัพท์ก่อนทุกคำครับ"
+        const val TEXT_THAI_WITHOUT_SPACE =
+            "Afghanistan~Albania~Algeria~Andorra~Angola~Antigua~Barbuda~Argentina~Armenia~Australia~Austria~Azerbaijan~Bahamas~Bahrain~Bangladesh~Barbados~Belarus~Belgium~Belize~Benin~Bhutan~Bolivia~BosniaHerzegovina~Botswana~Brazil~Brunei~Bulgaria~Burkina~Faso~Burundi~Cambodia~Cameroon~Canada~Cape~Verde~Central~African~Republic~Chad~Chile~China~Colombia~Comorros~Congo~Costa~Rica~Cote~dIvoire~Croatia~Cuba~Cyprus~Czech~Republic~Democratic~Republic~of~the~Congo~Denmark~Djibouti~Dominica~Dominican~Republic~East~Timor~Ecuador~Egypt~El~Salvador~Equatorial~Guinea~Eritrea~Estonia~Ethiopia~Fiji~Finland~France~Gabon~Gambia~Georgia~Germany~Ghana~Greece~Grenada~Guatemala~Guinea~Guinea-"
+        const val TEXT_THAI_NOT_ENOUGH_SPACE = "Start\n1\n2\n3\n4\n5\nEnd"
+        const val TEXT_THAI_NOT_ENOUGH_CHAR = "D\ne\ns\ntina\nt\ni\ni\no\nn"
+        const val TEXT_THAI_ENOUGH_MAX_LINE = "How best to make use of comments is\n" +
                 "In computer programming, a comment\n" +
                 "A good title must be compelling \n" +
                 "Apart from that, the title page\n" +
@@ -64,7 +86,11 @@ class MainActivity : AppCompatActivity() {
             DynamicTypeData.SampleDate(TEXT_ENG_NOT_ENOUGH_CHAR),
             DynamicTypeData.SampleDate(TEXT_ENG_WITHOUT_SPACE)
         )
+        private val listNewSampleData = listOf<DynamicTypeData.SampleDate>(
+            DynamicTypeData.SampleDate(TEXT_LONG_WITHOUT_NEW_LINE)
+        )
         private val fakeList = listOf<DynamicTypeData>(
+            DynamicTypeData.SampleNewData(listNewSampleData),
             DynamicTypeData.RecommendData(listSampleDate),
             DynamicTypeData.ImageData()
         )
@@ -119,6 +145,11 @@ class MainActivity : AppCompatActivity() {
 
                 }
 
+                is DynamicTypeData.SampleNewData -> {
+                    val data = fakeList[position] as DynamicTypeData.SampleNewData
+                    val sampleHolder = holder as MyViewHolderNewData
+                }
+
                 is DynamicTypeData.RecommendData -> {
                     val data = fakeList[position] as DynamicTypeData.RecommendData
                     val sampleHolder = holder as MyViewHolderRecommendV2
@@ -133,7 +164,8 @@ class MainActivity : AppCompatActivity() {
 
         override fun getItemViewType(position: Int): Int {
             return when (fakeList[position]) {
-                is DynamicTypeData.SampleDate -> R.layout.item_sample_expand
+                is DynamicTypeData.SampleNewData -> R.layout.item_sample_expand
+//                is DynamicTypeData.SampleDate -> R.layout.item_sample_expand
                 is DynamicTypeData.RecommendData -> R.layout.item_recommand_detail
                 else -> R.layout.item_image_detail
             }
@@ -166,6 +198,9 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    class MyViewHolderTestNewMaxLine(private val iv: View) : RecyclerView.ViewHolder(iv) {
+
+    }
 
     class MyViewHolderRecommendV2(private val iv: View) : RecyclerView.ViewHolder(iv) {
 
@@ -200,11 +235,43 @@ class MainActivity : AppCompatActivity() {
 //                }
                 containMainView.addView(popup)
             }
-
-
         }
+    }
 
+    class MyViewHolderNewData(private val iv: View) : RecyclerView.ViewHolder(iv) {
 
+        var containMainView: LinearLayout = iv.containerMain
+
+        fun binData(
+            list: List<DynamicTypeData.SampleDate>,
+            onUpdate: (newState: Boolean, index: Int) -> Unit
+        ) {
+            list.forEachIndexed { index, dynamicTypeData ->
+                val inflater =
+                    iv.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+
+                val popup = inflater.inflate(R.layout.item_recommand_detail, null)
+                val expTextView = popup.findViewById<CustomExpandText>(R.id.tvExp)
+                expTextView.setExpandableText(dynamicTypeData.deteail)
+                expTextView.isExpand = false
+                expTextView.onStateChangeListener = { oldState, newState ->
+                    onUpdate.invoke(newState, index)
+                }
+//                val expTextView2 = popup.findViewById<TextView>(R.id.testja)
+//                expTextView2.text = dynamicTypeData.deteail
+//                if (x==10){
+//                    val divider = popup.findViewById<View>(R.id.divider)
+//                    divider.visibility = View.GONE
+//                }
+
+//                val expTextView3 = popup.findViewById<CustomTextButtonExpand>(R.id.tvCustom)
+//                expTextView3.setExpandableText(dynamicTypeData.deteail)
+//                expTextView3.onStateChangeListener = { oldState, newState ->
+//                    onUpdate.invoke(newState, index)
+//                }
+                containMainView.addView(popup)
+            }
+        }
     }
 
     sealed class DynamicTypeData {
@@ -214,6 +281,9 @@ class MainActivity : AppCompatActivity() {
         data class ImageData(val deteail: String = "") : DynamicTypeData()
 
         data class RecommendData(val list: List<SampleDate>) : DynamicTypeData()
+
+        data class SampleNewData(val list: List<SampleDate>) :
+            DynamicTypeData()
 
     }
 }
